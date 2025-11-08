b11f49df2c69c916e52343af1c406aaf90b310dc
Vira <viraproject1308@gmail.com>
2025-11-08 19:01:05 +0200

    get weather api

--- weather/views.py
+++ weather/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
+import requests
 
 def index(request):
-    return render(request, 'weather/index.html')
+    api_key = "f8fb469678bf1d6bc87f2d9f43340d6b"
+    city = "London"
+
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}"
+
+    print(url)
+
+    res = requests.get(url).json()
+
+    city_info = {
+        'city': city,
+        'temp': res['main']['temp'],
+        'icon': res['weather'][0]['icon']
+    }
+
+    context = {
+        'info': city_info
+    }
+
+    return render(request, 'weather/index.html', context)
